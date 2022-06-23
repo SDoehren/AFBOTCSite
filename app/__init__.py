47705f378205a93e5f48014fb572f32367bfa9a6
SDoehren <samdoehren@gmail.com>
2022-06-23 21:52:08 +0100

    1

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask,url_for
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'FD29E48FB911DD1A858D1FCF246E3'
